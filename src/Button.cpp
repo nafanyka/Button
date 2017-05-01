4ff51898bf1b3f764959cadb549e0fab890c2cbf
nafanyka <nafanyka@gmail.com>
2017-05-01 18:03:40 +0300

    init

---
+++ src/Button.cpp
@@ -0,0 +1,10 @@
+//
+// Created by nafanya on 01.05.2017.
+//
+#include <Arduino.h>
+#include "Button.h"
+
+namespace NAF {
+
+
+}
---
+++ src/Button.h
@@ -0,0 +1,14 @@
+//
+// Created by nafanya on 01.05.2017.
+//
+
+#ifndef NAF_BUTTON_H
+#define NAF_BUTTON_H
+
+namespace NAF {
+    class Button {
+
+    };
+}
+
+#endif //NAF_BUTTON_H
