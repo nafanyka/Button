b9d8fbdb95ef618f80f5a385d13fd5e29e4cc0f1
nafanyka <nafanyka@gmail.com>
2017-06-28 21:01:35 +0300

    sdf

--- src/Button.h
+++ src/Button.h
@@ -5,9 +5,30 @@
 #ifndef NAF_BUTTON_H
 #define NAF_BUTTON_H
 
+#include "../../Timer/src/Timer.h"
+
 namespace NAF {
     class Button {
-
+    public:
+        Button(int pin);                    // constructor: input pullup enabled by default
+        Button(int pin, bool input_pullup); // overloaded constructor: flag available to disable input pullup
+        void setDebounceTimeout(unsigned long value);
+        bool isPressed();
+        bool isReleased();
+        bool onPressed();
+        bool onReleased();
+        void invertReading();
+    private:
+        int _pin;
+        unsigned long _debounce_timeout = 10; // ms
+        bool _has_been_pressed  = false;
+        bool _has_been_released = false;
+        bool _invert = true;
+        bool _temp_reading;
+        void _inputPullup();
+        void _disableInputPullup();
+        Timer _pressed_debounce;
+        Timer _released_debounce;
     };
 }
 
